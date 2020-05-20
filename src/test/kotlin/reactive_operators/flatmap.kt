3e2289236a755c36c3cfd4d3d76362dc57097d6c
Mihai Badea <mihai.badea@ocado.com>
2020-05-20 15:17:04 +0100

    flatmap

--- src/test/kotlin/reactive_operators/flatmap.kt
+++ src/test/kotlin/reactive_operators/flatmap.kt
@@ -9,6 +9,14 @@ class flatmap {
     fun flatmap_letters() {
         Observable.just<String>("one", "two", "three")
             .flatMap { Observable.fromIterable(it.split("")) }
-            .subscribe { println("$it")}
+            .subscribe { print("$it ")}
+    }
+
+    @Test
+    fun flatmap_parse() {
+        Observable.just("1 time I found 2 fish in 3 lakes with 4 eyes.", "99 bottles and 15 crew")
+            .flatMap { Observable.fromIterable(it.split(" ")) }
+            .filter { it.matches(Regex("[0-9]+"))}
+            .subscribe { print("$it ")}
     }
 }
